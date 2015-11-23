fc6b73742b8f92c7c2607d5120c4eed187dbf53c
avinci1015 <Anthony.vinci@quinnipiac.edu>
2015-11-23 16:47:55 +0000

    Add user delete view

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^bar/(?P<bar_pk>\d+)/response/delete/(?P<response_pk>\d+)/$', login_required(ResponseDeleteView.as_view()), name='response_delete'), 
     url(r'^vote/$', login_required(VoteFormView.as_view()), name='vote'),   
     url(r'^user/(?P<slug>\w+)/$', login_required(UserDetailView.as_view()), name='user_detail'),
-    url(r'^user/update/(?P<slug>\w+)/$', login_required(UserUpdateView.as_view()), name='user_update'),         
+    url(r'^user/update/(?P<slug>\w+)/$', login_required(UserUpdateView.as_view()), name='user_update'),
+    url(r'^user/delete/(?P<slug>\w+)/$', login_required(UserDeleteView.as_view()), name='user_delete'),       
     url(r'^bar/(?P<bar_pk>\d+)/response/update/(?P<response_pk>\d+)/$', login_required(ResponseUpdateView.as_view()), name='response_update'),
 )    
